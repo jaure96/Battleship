50335b3f66de5602082302a3b2f1ef58d35aa0b1
Xabier Jauregi <giamexak@gmail.com>
2025-10-08 09:39:11 +0200

    create battle screen

--- app/create_battle.tsx
+++ app/create_battle.tsx
@@ -2,7 +2,7 @@ import GameHeader from "@/components/GameHeader";
 import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
 import { useNavigation } from "expo-router";
 import { useCallback, useState } from "react";
-import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Switch, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const CreateBattle = () => {
@@ -10,6 +10,7 @@ const CreateBattle = () => {
   const { top } = useSafeAreaInsets();
 
   const [roomName, setRoomName] = useState("");
+  const [isPublic, setIsPublic] = useState(true);
 
   const onRoomNameChange = useCallback((val: string) => {
     setRoomName(val);
@@ -36,10 +37,10 @@ const CreateBattle = () => {
 
       <View className="w-full mt-10 px-10 py-5 bg-black/80 rounded-xl">
         <View className="flex justify-center items-center  ">
-          <MaterialCommunityIcons name="crown" color="#ffcc33" size={64} />
+          <MaterialCommunityIcons name="crown" color="#ffcc33" size={55} />
         </View>
         <View className="flex-col mt-5">
-          <Text className="font-mono text-lg color-white">Room name:</Text>
+          <Text className="font-mono text-md color-white">Room name:</Text>
           <TextInput
             value={roomName}
             className="bg-slate-400/30 color-white/50 rounded-sm font-mono"
@@ -52,7 +53,33 @@ const CreateBattle = () => {
         </View>
 
         {/*Public or private room */}
-        <View></View>
+        <View className="flex-row items-center justify-between p-4 my-3 border-2 border-border bg-muted/20">
+          <View className="flex-row gap-2">
+            <View className="flex-row justify-center align-middle items-center  gap-2">
+              {isPublic ? (
+                <Ionicons name="eye" color={"#ffcc33"} size={20} />
+              ) : (
+                <Ionicons name="eye-off" color={"gray"} size={20} />
+              )}
+            </View>
+            <View className="flex-col justify-items-center align-middle">
+              <Text className="text-sm font-mono font-bold text-foreground">
+                Public room
+              </Text>
+              <Text className="text-xs text-muted-foreground font-mono">
+                {isPublic ? "Other players can find it" : "Code access only"}
+              </Text>
+            </View>
+          </View>
+          <View className="flex-row items-center gap-3">
+            <Switch
+              trackColor={{ false: "#767577", true: "#0099e6" }}
+              thumbColor={isPublic ? "#204060" : "#a8abb3"}
+              onValueChange={() => setIsPublic((prev) => !prev)}
+              value={isPublic}
+            />
+          </View>
+        </View>
 
         {/*Button*/}
         <TouchableOpacity
